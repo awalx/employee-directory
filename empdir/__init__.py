7d48f082070d4d085f6b83d58f632c606967031f
Andre Alexander <awalx1@gmail.com>
2019-02-26 19:33:03 -0500

    Install flask-SQLAlchemy and initialize database

--- empdir/__init__.py
+++ empdir/__init__.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+DATABASE = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+db = SQLAlchemy(app)
+
 Bootstrap(app)
 
 import empdir.views
