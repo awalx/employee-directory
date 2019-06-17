8679dab38a59f6f0c01fe0b874901cb0ff363194
Andre Alexander <awalx1@gmail.com>
2019-06-17 11:38:15 -0400

    Add DB file and setup

--- empdir/__init__.py
+++ empdir/__init__.py
@@ -1,12 +1,12 @@
 import os
+from config import Config
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-DATABASE = os.environ.get('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 db = SQLAlchemy(app)
 
 Bootstrap(app)
