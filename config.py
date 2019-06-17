6ccfa754d488edc73474082bc3416e51f0cf6a51
Andre Alexander <awalx1@gmail.com>
2019-06-17 11:43:37 -0400

    move config.py to fix error

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-my-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, '')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'chinook.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
